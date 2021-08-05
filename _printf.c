ddd89154a93f74ee25192761135deb2545befe3f
treserio <treserio@hotmail.com>
2021-08-05 09:22:46 -0500

    workingn through it

--- _printf.c
+++ _printf.c
@@ -108,7 +108,45 @@ void flags_reset(flag_list *flagz)
 int flag_set(const char *c, flag_list *flagz, int *i, int *D)
 {
 	char *mods = "hlX", *ops = "bcdio%suxX";
-	int j = 0;
+	int j, ordr = *i + 1;
+
+	if (!*flagz.op && *c == '%')
+	{
+		(*flagz).op = order;
+		++c;
+		++*i;
+	}
+	}
+	for (j = 0; (*flagz).op && mods[j]; ++j)
+	{
+		if (mods[j] == *c)
+		{
+			if (j == 0)
+				(*flagz).h = ++ordr;
+				return (0);
+			else if (j == 1)
+				(*flagz).l = ++ordr;
+				return (0);
+			else if (j == 2)
+				(*flagz).X = ++ordr;
+				return (0);
+		}
+	}
+	for (j = 0; (*flagz).op && ops[j]; ++j)
+		is (*ops[j] == *c)
+			return (0);
+	
+	/* create a global flag buffer and add to it until it's end of buffer */
+
+	if ((*flagz).op && *c == ' ')
+		return (0);
+	else if ((*flagz).op)
+		*D += _putchar('%');
+
+
+
+
+
 
 	if (!(*flagz).op && *c == '%')
 	{
@@ -135,10 +173,50 @@ int flag_set(const char *c, flag_list *flagz, int *i, int *D)
 	for (j = 0; ops[j]; ++j)
 		if (*c == ops[j])
 			return (0);
-if ((*flagz).op && *c != ' ' && ((*flagz).h || (*flagz).l || (*flagz).X))
+	if ((*flagz).op && *c != ' ' && ((*flagz).h || (*flagz).l || (*flagz).X))
 	{
 		*D += _putchar('%');
 		flags_reset(flagz);
 	}
 	return (0);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
